4dbb5fe556acc854db1c68f7c88fad4b79d1ab63
lzanela <106616119+lzanela@users.noreply.github.com>
2023-05-17 11:52:36 -0300

    Add files via upload

---
+++ Clase 17.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+huevos = pd.read_csv("huevos.csv")
+
+# Generamos los dataframes para las variables
+
+x = pd.DataFrame(huevos["RU"])
+y = pd.DataFrame(huevos["DI"])
+
+# Utilizamos el modelo de regresión lineal
+
+model = linear_model.LinearRegression()
+
+model.fit(x,y)
+
+# Visualizamos los coeficientes determinodos por el modelo
+
+b0 = model.coef_
+b1= model.intercept_
+
+print(b0)
+print(b1)
+
+#Graficamos los datos y la recta generada por el modelo
+
+plt.scatter(x,y)
+
+x1 = range(-100,1850,100)
+y1 = b0[0][0]*x1+b1[0]
+
+plt.plot(x1, y1, color = "red")
+plt.title("Regresión lineal")
+plt.xlabel("Dosis de RU (ug/huevo)")
+plt.ylabel("Índice de daño")
+
+# El r_cuadrado es un coeficiente de 
+
+r_cuadrado = model.score(x,y)
+print(r_cuadrado)
